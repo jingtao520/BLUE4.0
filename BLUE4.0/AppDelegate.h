fe546ca735f26bc2b70ddce4efb517280d93e926
hxlz <hxlz@hxlz.local>
2015-06-08 16:08:06 +0800

    Initial Commit

---
+++ BLUE4.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLUE4.0
+//
+//  Created by hxlz on 15/6/8.
+//  Copyright (c) 2015年 hxlz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
