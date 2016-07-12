e325daf607b8e22f3d841aefe70da6da0d63df07
snb <snb@snbdeMacBook-Pro.local>
2016-07-12 09:16:16 +0800

    xiuggai

--- BLUE4.0/ViewController.m
+++ BLUE4.0/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 测试的 git 命令
+    
 }
 
 - (void)didReceiveMemoryWarning {
