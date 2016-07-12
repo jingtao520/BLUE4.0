db98f5eceabdfccf25df43741c49db893695528f
snb <snb@snbdeMacBook-Pro.local>
2016-07-12 09:24:59 +0800

    增加了一个新的VC控制器

---
+++ TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  BLUE4.0
+//
+//  Created by snb on 16/7/12.
+//  Copyright © 2016年 hxlz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ TestViewController.m
@@ -0,0 +1,44 @@
+//
+//  TestViewController.m
+//  BLUE4.0
+//
+//  Created by snb on 16/7/12.
+//  Copyright © 2016年 hxlz. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    
+    self.view.backgroundColor = [UIColor redColor];
+    NSLog(@"测试--git--用的了");
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
